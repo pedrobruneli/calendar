c337bac09e2efc9d983c0c860e8e8b2493087c15
pedrobruneli <smurfdebruneli@hotmail.com>
2024-03-30 19:36:07 -0300

    feat: implement responsivity on calendar page

--- src/app/calendar/[id]/page.tsx
+++ src/app/calendar/[id]/page.tsx
@@ -5,6 +5,7 @@ import { Clock } from "lucide-react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useState } from "react";
 import { HourSelection } from "./components/hour-selection";
+import { cn } from "@/lib/utils";
 
 type CalendarProps = {
   params: {
@@ -14,20 +15,23 @@ type CalendarProps = {
 
 export default function CalendarPage({ params }: CalendarProps) {
   const [date, setDate] = useState<Date | undefined>(new Date());
+  const [showMore, setShowMore] = useState(false);
   return (
-    <main className="h-dvh flex items-center justify-center">
-      <Card className="flex w-11/12 max-w-[800px] max-h-[700px] flex-1">
-        <ScrollArea className="w-full">
-          <div className="flex flex-col gap-3 border-r border-border p-6">
-            <h1 className="text-xl font-bold">1:1 Bruneli</h1>
-            <div className="flex items-center gap-2">
-              <Clock color="rgba(26,26,26,0.61)" size={20}></Clock>
-              <span>30 min</span>
-            </div>
-            <div>
-              {[...new Array(20)].map((item, index) => {
-                return (
-                  <>
+    <main className="md:h-screen flex items-center justify-center p-5">
+      <Card className="flex flex-col w-11/12 h-full max-w-[800px] md:max-h-[700px] flex-1 md:flex-row">
+        <div className="border-b p-6 md:border-r md:border-b-0">
+          <ScrollArea
+            className={cn("w-full h-full", !showMore && "max-h-[700px]")}
+          >
+            <div className="flex flex-col gap-3">
+              <h1 className="text-xl font-bold">1:1 Bruneli</h1>
+              <div className="flex items-center gap-2">
+                <Clock color="rgba(26,26,26,0.61)" size={20}></Clock>
+                <span>30 min</span>
+              </div>
+              <div>
+                {[...new Array(20)].map((item, index) => {
+                  return (
                     <p key={index}>
                       Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                       Qui officia voluptate ea eius aliquid ipsam ut
@@ -35,13 +39,19 @@ export default function CalendarPage({ params }: CalendarProps) {
                       dicta a molestias corrupti voluptatibus eius
                       exercitationem quos.
                     </p>
-                  </>
-                );
-              })}
+                  );
+                })}
+              </div>
             </div>
-          </div>
-        </ScrollArea>
-        <div className="flex flex-col gap-3 items-start p-6 w-full">
+          </ScrollArea>
+          <span
+            className="font-bold cursor-pointer md:hidden"
+            onClick={() => setShowMore(!showMore)}
+          >
+            Show {showMore ? "less" : "more"}
+          </span>
+        </div>
+        <div className="flex flex-col gap-3 items-start p-6 w-full h-full">
           <h1 className="text-lg font-bold">Selecione uma data</h1>
           <Calendar
             mode="single"
